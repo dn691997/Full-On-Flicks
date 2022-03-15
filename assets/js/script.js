89226d64ea681dc6d049ae1853fe22e6177295fe
Deck Stream <tneswick2@verizon.net>
2022-03-14 22:54:39 -0700

    javascript logic for randomizer btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // regular api key = k_ix11kdvq
 
+// dom variables
 var movieTitleEl = document.getElementById("movie-title");
 var posterEl = document.getElementById("poster");
 var taglineEl = document.getElementById("tagline")
@@ -7,13 +8,17 @@ var plotEl = document.getElementById("plot");
 var castListEl = document.getElementById("cast-list");
 var searchInputEl = document.getElementById("search-bar");
 var searchBtn = document.getElementById("search-btn");
-var randomizerBtn = document.getElementById("random-btn");
-var directorsEl = document.getElementById("directors-list");
+var randomizerBtn = document.getElementById("randomizer-btn");
+var directorsList = document.getElementById("directors-list");
 var writersEl = document.getElementById("writers-list");
 var ratingEl = document.getElementById("rating");
 var runtimeEl = document.getElementById("runtime");
 var metacriticEl = document.getElementById("metacritic");
 var buttonsContainer = document.getElementById("btn-container");
+var directionTitle = document.getElementById("direction");
+var writerTitle = document.getElementById("writers");
+var metaCritic = document.getElementById("meta-critic");
+var runtimeTitle = document.getElementById("run-time");
 
 var getSearchTerm = function () {
     var searchTerm = searchInputEl.value;
@@ -31,12 +36,9 @@ var getTitleId = function (searchTerm) {
     fetch(apiURL)
     .then(function (response) {
         if (response.ok) {
-            console.log(response);
             response.json().then(function (data) {
-                console.log(data);
                 var titleId = data.results[0].id
-                console.log(titleId);
-                getMovieInfo(titleId);
+                getTitleInfo(titleId);
             })
         } else {
             console.log("Error: There was a problem with your request");
@@ -45,41 +47,140 @@ var getTitleId = function (searchTerm) {
     })
 }
 
-var getMovieInfo = function (titleId) {
+var getTitleInfo = function (titleId) {
     var apiUrl = "https://imdb-api.com/en/API/Title/k_ix11kdvq/" + titleId;
-    console.log(apiUrl);
     fetch(apiUrl)
     .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
+                // clear out last movie
                 castListEl.innerHTML = "";
+                directorsList.innerHTML = "";
+                writersEl.innerHTML = "";
+                // fill out values
                 posterEl.setAttribute("src", data.image)
                 movieTitleEl.textContent = data.fullTitle;
                 plotEl.textContent = data.plot;
-                taglineEl.textContent = data.tagline;
+                taglineEl.textContent = '"' + data.tagline + '"';
+                
+
                 
                 if (data.actorList[0]) {
                     for (var i = 0; i < 6; i++) {
-                        var castMemberName = data.actorList[i].name;
-                        var castMemberId = data.actorList[i].id;
-                        var castMemberImage = data.actorList[i].image;
-
-                        // create anchor element with it's inner html elements
-                        var castAnchorEl = document.createElement("a");
-                        castAnchorEl.setAttribute("src", "https://www.imdb.com/name/" + castMemberId + "/");
-                        castAnchorEl.setAttribute("target", "_blank");
-                        castAnchorEl.innerHTML = "<figure><img src='" + castMemberImage + "' alt='" + castMemberName + "' width='128px'/><figcaption>Leonardo<br/>DiCaprio</figcaption></figure>";
-                        castListEl.appendChild(castAnchorEl);
+                        if (data.actorList[i]) {
+                            var castMemberName = data.actorList[i].name;
+                            var castMemberId = data.actorList[i].id;
+                            var castMemberImage = data.actorList[i].image;
+                            // tool to split up name
+                            var formattedName = castMemberName.replaceAll(" ", "<br/>");
+    
+                            // create anchor element with it's inner html elements
+                            var castAnchorEl = document.createElement("a");
+                            castAnchorEl.setAttribute("src", "https://www.imdb.com/name/" + castMemberId + "/");
+                            castAnchorEl.setAttribute("target", "_blank");
+                            castAnchorEl.innerHTML = "<figure><img src='" + castMemberImage + "' alt='" + castMemberName + "' width='128px'/><figcaption>" + formattedName + "</figcaption></figure>";
+                            castListEl.appendChild(castAnchorEl);
+                        } else {
+                            return;
+                        }
                     } 
                 } else {
                     castListEl.textContent = "No cast to display";
+                }
+                // directors list
+                if (data.directorList[0]) {
+                    for (var i = 0; i < data.directorList.length; i++) {
+                        var directorListEl = document.createElement("li");
+                        directorListEl.classList.add("has-text-white-bis", "is-size-6");
+                        directorListEl.textContent = data.directorList[i].name;
+                        directorsList.appendChild(directorListEl);
+                    }
+                } else {
+                    directionTitle.classList.add("is-invisible")
+                }
+                // writers list
+                if (data.writerList[0]) {
+                    for (var i=0; i<data.writerList;i++) {
+                        var writer = document.createElement("li");
+                        writer.classList.add("has-text-white-bis", "is-size-6");
+                        writer.textContent = data.writerList[i].name;
+                        writersEl.appendChild(writer);
+                    }
+                } else {
+                    writerTitle.classList.add("is-invisible");
+                }
+
+                if (data.contentRating) {
+                    ratingEl.textContent = "Rated: " + data.contentRating
+                } else {
+                    ratingEl.classList.add("is-invisble")
+                }
+
+                if (data.runtimeStr) {
+                    runtimeEl.textContent = data.runtimeStr
+                } else {
+                    runtimeTitle.classList.add("is-invisible")
+                }
+
+                if (data.metacriticRating) {
+                    metacriticEl.textContent = data.metacriticRating
+                } else {
+                    metaCritic.classList.add("is-invisible");
                 }
             })
         }
     })
 }
 
-// "name" external link for <a> https://www.imdb.com/name/ nm0229694 /
-// <img src="https://imdb-api.com/images/original/MV5BMjI0MTg3MzI0M15BMl5BanBnXkFtZTcwMzQyODU2Mw@@._V1_Ratio1.0000_AL_.jpg" alt="Leonardo DiCaprio" width="128px"/>
+var randomNumber = function () {
+    // random number up to 250
+    var arrayNumber = Math.floor(Math.random() * 250);
+    if (arrayNumber === 0) {
+        arrayNumber = 1;
+    }
+    // if even, random movie
+    if (arrayNumber % 2 == 0) {
+        getRandomMovie(arrayNumber);
+    } else {
+        getRandomTV(arrayNumber);
+    }
+}
 
-// getTitleId("Inception");
+var getRandomMovie = function (number) {
+    fetch("https://imdb-api.com/en/API/Top250Movies/k_ix11kdvq")
+    .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                var movieId = data.items[number].id;
+                getTitleInfo(movieId);
+            })
+        } else {
+            alert("There was a problem with your request, please try again")
+        }
+    })
+}
+
+var getRandomTV = function (number) {
+    fetch("https://imdb-api.com/en/API/Top250TVs/k_ix11kdvq")
+    .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                var showId = data.items[number].id;
+                getTitleInfo(showId);
+            })
+        } else {
+            alert("There was a problem with your request, please try again")
+        }
+    })
+}
+
+// event listeners
+randomizerBtn.addEventListener("click", randomNumber);
+searchBtn.addEventListener("click", getSearchTerm);
+
+searchInputEl.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.key === "Enter") {
+        getSearchTerm();
+    }
+})
